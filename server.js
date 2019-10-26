25d16cf8dd73427167373490c9dde207603890f2
Danik <ordinaryask@gmail.com>
2019-10-26 07:55:46 +0300

    'AdjSocketsDanik.'

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-const express = require('express');
+const express 	= require('express');
+const app 		= express();
+var server  	= require('http').createServer(app);
+var io			= require('socket.io').listen(server);
 
-const app = express();
+let port = process.env.PORT || 5000;
+server.listen(port, () => {
+	console.log(__dirname);
+	console.log("Listening Port " + port);
+});
 
 app.use(express.static(__dirname + '/dist'));
 app.use('/public',express.static('public'));
@@ -8,8 +15,17 @@ app.get('/', (req, res) => {
 	res.sendFile('index.html', { root : __dirname});
 });
 
-let port = process.env.PORT || 5000;
-app.listen(port, () => {
-	console.log(__dirname);
-	console.log("Listening Port " + port);
+users = [];
+connections = [];
+
+io.sockets.on('connection',function(socket){
+	console.log('Good connection');
+	var clientIp = socket.request.connection.remoteAddress;
+	console.log(clientIp);
+	connections.push(socket);
+
+	socket.on('disconnect',function(date){
+		connections.splice(connections.indexOf(socket), 1);
+		console.log('disconnect');
+	});
 });
