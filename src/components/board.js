82a8ac10b6cb2b402061c4bb25c8eef8e71db1cc
Danik <ordinaryask@gmail.com>
2019-10-28 21:39:04 +0300

    'figuresAdd.Danik'

--- src/components/board.js
+++ src/components/board.js
@@ -1,19 +1,80 @@
 import React from 'react';
 
 
-
+class Figure extends React.Component{
+	constructor(props) {
+		super(props);
+		this.state = {
+			FieldFig:this.props.fieldIndex,
+			nameFig:this.props.nameFigure,
+	    };
+	}
+	namedFig(){
+		return (this.state.nameFig + ' ' + 'figure');
+	}
+	render(){
+		const style = { backgroundColor: this.props.colorBg};
+		return(
+			<img className={this.namedFig()} src={this.props.figure}/>
+		)
+	}
+}
 
 class Field extends React.Component{
 	constructor(props) {
 	    super(props);
 	    this.state = {
-			index: this.props.index
+			figure:'public/images/figures/',
+			fieldIndex:this.props.index,
+			nameFigure:'',
 	    };	    
+	}
+	getFigure(){
+		var obj = {
+			1:{
+				a:'wR.png',
+				b:'wN.png',
+				c:'wB.png',
+				d:'wQ.png',
+				e:'wK.png',
+				f:'wB.png',
+				g:'wN.png',
+				h:'wR.png'
+			},
+			2:'wP.png',
+			7:'bP.png',
+			8:{
+				a:'bR.png',
+				b:'bN.png',
+				c:'bB.png',
+				d:'bQ.png',
+				e:'bK.png',
+				f:'bB.png',
+				g:'bN.png',
+				h:'bR.png'
+			},
+		};
+		var str1 = (this.state.fieldIndex)[1];
+		var str0 = (this.state.fieldIndex)[0];
+		switch (str1){
+			case '1':
+			case '8':
+				this.state.figure += obj[str1][str0];
+				this.state.nameFigure = obj[str1][str0].slice(0,2)
+				break;
+			case '2':
+			case '7':
+				this.state.figure += obj[str1];	
+				break;
+			default:
+				this.state.figure = '';	
+		}
+		return <Figure figure = {this.state.figure} fieldIndex = {this.state.fieldIndex} nameFigure = {this.state.nameFigure}/>;
 	}	
 	render(){
 		const style = { backgroundColor: this.props.colorBg};
 		return(
-			<div className = 'field' style = {style}></div>
+			<div className = 'field' style = {style} id = {this.props.index}> {this.getFigure()} </div>
 		)
 	}
 }
@@ -65,7 +126,6 @@ class Board extends React.Component{
 		return(
 			<div className = 'board' id = 'board'>
 				{this.FillField()}
-
 			</div>
 		)
 	}
